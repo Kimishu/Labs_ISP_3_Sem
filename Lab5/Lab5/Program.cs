fadb9a4a28dd629d183262d76d51ca4bc32ba1f3
Kimishu <46163424+Kimishu@users.noreply.github.com>
2021-11-08 19:17:30 +0300

    Lab5(Upgraded to Lab6)

---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,22 @@
+﻿using _053502_ANKUSHEV_LAB5.Entities;
+using System;
+using zz.Collections;
+using zz.Entities;
+
+namespace zz
+{
+    class Program
+    {
+        private static MainMenu mm = new();
+        
+        static void Main()
+        {
+            Journal events = new();
+            ATE at = new();
+
+            at.CallEvent += (entity,msg) => events.AddEvent(entity,msg);
+
+            mm.Menu(at);
+        }
+    }
+}
